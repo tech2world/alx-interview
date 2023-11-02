2120dab6820460d9bc56cf7876ecc1630a843c17
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-11-02 23:00:38 +0100

    updated 0-nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -24,7 +24,7 @@ julien@ubuntu:~/0x08. N Queens$ ./0-nqueens.py 6
 [[0, 2], [1, 5], [2, 1], [3, 4], [4, 0], [5, 3]]
 [[0, 3], [1, 0], [2, 4], [3, 1], [4, 5], [5, 2]]
 [[0, 4], [1, 2], [2, 0], [3, 5], [4, 3], [5, 1]]
-julien@ubuntu:~/0x08. N Queens$ 
+julien@ubuntu:~/0x08. N Queens$
 
 You don’t have to print the solutions in a specific order
 You are only allowed to import the sys module
