b9d0abe15da0528793ddf78f838b9d1e556179b3
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-11-10 00:01:17 +0100

    updated the script to use async

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -9,34 +9,50 @@ if (process.argv.length !== 3) {
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-request(url, (error, response, body) => {
-  if (error) {
+const getCharacter = (characterUrl) => {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+
+      if (response.statusCode !== 200) {
+        reject(`Invalid status code: ${response.statusCode}`);
+      }
+
+      const character = JSON.parse(body);
+      resolve(character.name);
+    });
+  });
+};
+
+const fetchCharacters = async () => {
+  try {
+    const response = await new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+        }
+
+        if (response.statusCode !== 200) {
+          reject(`Invalid response code: ${response.statusCode}`);
+        }
+
+        resolve(body);
+      });
+    });
+
+    const movieData = JSON.parse(response);
+    const characters = movieData.characters;
+
+    for (let i = 0; i < characters.length; i++) {
+      const characterName = await getCharacter(characters[i]);
+      console.log(characterName);
+    }
+  } catch (error) {
     console.error('Error:', error);
     process.exit(1);
   }
+};
 
-  if (response.statusCode !== 200) {
-    console.error('Invalid response code', response.statusCode);
-    process.exit(1);
-  }
-
-  const MovieData = JSON.parse(body);
-  const characters = MovieData.characters;
-
-  characters.forEach(characterUrl => {
-    request(characterUrl, (charError, charResposne, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-        return;
-      }
-
-      if (charResposne.statusCode !== 200) {
-        console.error('Invalid status code', charResposne);
-        return;
-      }
-
-      const character = JSON.parse(charBody);
-      console.log(character.name);
-    });
-  });
-});
+fetchCharacters();
