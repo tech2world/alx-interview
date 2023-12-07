08934426f87ede7f9195fa7cebf836328674ae26
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-12-07 01:04:55 +0100

    updated code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
 Module: island_perimeter
-Description: Provides a function to calculate the perimeter of an island in a 2D grid.
+Description: Provides a function to calculate the perimeter
+of an island in  2D grid.
 """
+
 
 def island_perimeter(grid):
     """
