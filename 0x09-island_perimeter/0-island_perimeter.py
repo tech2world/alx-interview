39a4bb3525654792352ee5e1fd0f328da50f29bd
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-12-07 01:01:59 +0100

    updated code

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python3
+"""
+Module: island_perimeter
+Description: Provides a function to calculate the perimeter of an island in a 2D grid.
+"""
 
 def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in the grid.
+
+    Args:
+        grid (List[List[int]]): A 2D grid representing an island.
+
+    Returns:
+        int: The perimeter of the island.
+
+    Example:
+        grid = [
+            [0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0]
+        ]
+        island_perimeter(grid)  # Output: 12
+    """
     perimeter = 0
 
     # Iterate through each cell in the grid
